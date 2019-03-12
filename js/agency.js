25271f6603c98438f578814725e0d3f3bf59a254
MeeYoon <auseyoo@gmail.com>
2019-03-13 02:21:54 +0900

    잘될거야...

--- js/agency.js
+++ js/agency.js
@@ -28,7 +28,7 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
+    if ($("#mainNav").offset().top > 700) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
